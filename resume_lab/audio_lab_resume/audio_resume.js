d1b8177b1ad99061f4638cbf9ddcbca4b859bedd
Royta-Iftakher <98833053+Royta-Iftakher@users.noreply.github.com>
2024-05-19 05:22:38 -0400

    added wick_interactive and pwa

---
+++ resume_lab/audio_lab_resume/audio_resume.js
@@ -0,0 +1,43 @@
+
+var sinkAudio = document.getElementById("sink");
+function playSink(){
+    sinkAudio.play();
+}
+
+var mixerAudio = document.getElementById("mixer");
+function playMixer(){
+    mixerAudio.play();
+}
+
+var kettleAudio = document.getElementById("kettle");
+function playKettle(){
+    kettleAudio.play();
+}
+
+var potAudio = document.getElementById("pot");
+function playPot(){
+    potAudio.play();
+}
+
+var cabinetAudio = document.getElementById("cabinet");
+function playCabinet(){
+    cabinetAudio.play();
+}
+
+var timerAudio = document.getElementById("timer");
+function playTimer(){
+    timerAudio.play();
+}
+
+var clothAudio = document.getElementById("cloth");
+function playCloth(){
+    clothAudio.play();
+}
+
+var mainAudio = document.getElementById("main");
+function playMain(){
+    mainAudio.play();
+}
+function stopMain(){
+    mainAudio.pause();
+}
